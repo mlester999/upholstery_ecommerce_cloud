6c24421eadbf314f9f02f77c721783f457ed237a
Mark Lester Acak <mister6virgo@gmail.com>
2023-11-16 23:00:52 +0800

    added validation for phone number and email

--- src/components/admins/AddNewAdminFields.tsx
+++ src/components/admins/AddNewAdminFields.tsx
@@ -1,5 +1,5 @@
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import { Formik } from "formik";
+import * as Yup from "yup";
 import {
   Box,
   Card,
@@ -11,24 +11,24 @@ import {
   FormControl,
   FormHelperText,
   Unstable_Grid2 as Grid,
-} from '@mui/material';
-import { LoadingButton } from '@mui/lab';
-import Colors from '../../constants/Colors';
-import { useNavigate } from 'react-router-dom';
-import { useCreateAdminMutation } from '../../services/crud-admin';
-import { toast } from 'react-toastify';
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import Colors from "../../constants/Colors";
+import { useNavigate } from "react-router-dom";
+import { useCreateAdminMutation } from "../../services/crud-admin";
+import { toast } from "react-toastify";
 
 const AddNewAdminFields = () => {
   const [createAdmin, { isLoading }] = useCreateAdminMutation();
   const navigate = useNavigate();
 
   const initialValues = {
-    first_name: '',
-    middle_name: '',
-    last_name: '',
-    email: '',
-    contact_number: '',
-    gender: '',
+    first_name: "",
+    middle_name: "",
+    last_name: "",
+    email: "",
+    contact_number: "",
+    gender: "",
   };
 
   return (
@@ -36,36 +36,51 @@ const AddNewAdminFields = () => {
       <Formik
         initialValues={initialValues}
         validationSchema={Yup.object().shape({
-          first_name: Yup.string().required('First Name is required'),
+          first_name: Yup.string().required("First Name is required"),
           middle_name: Yup.string().notRequired(),
-          last_name: Yup.string().required('Last Name is required'),
+          last_name: Yup.string().required("Last Name is required"),
           email: Yup.string()
-            .email('Must be a valid email')
+            .email("Must be a valid email")
             .max(255)
-            .required('Email Address is required'),
+            .required("Email Address is required"),
           contact_number: Yup.string()
-            .matches(/^(09\d{9})?$/, 'Invalid Contact Number')
-            .required('Contact Number is required'),
+            .matches(/^(09\d{9})?$/, "Invalid Contact Number")
+            .required("Contact Number is required"),
           gender: Yup.string()
-            .oneOf(['Male', 'Female'], 'Invalid gender')
-            .required('Gender is required'),
+            .oneOf(["Male", "Female"], "Invalid gender")
+            .required("Gender is required"),
         })}
         onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
           createAdmin(values)
             .unwrap()
             .then((payload) => {
-              navigate('/portal/admins');
+              navigate("/portal/admins");
 
-              toast.success('Added Admin Successfully!', {
-                position: 'top-right',
+              toast.success("Added Admin Successfully!", {
+                position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 progress: undefined,
-                theme: 'light',
+                theme: "light",
               });
             })
-            .catch((error) => setErrors({ email: error.data?.message }));
+            .catch((error) => {
+              if (
+                error.data?.message ===
+                  "The email address that you provided is already taken." ||
+                "Failed creating a user."
+              ) {
+                setErrors({ email: error.data?.message });
+              }
+
+              if (
+                error.data?.message ===
+                "The contact number that you provided is already taken."
+              ) {
+                setErrors({ contact_number: error.data?.message });
+              }
+            });
         }}
       >
         {({
@@ -82,8 +97,8 @@ const AddNewAdminFields = () => {
           <form noValidate onSubmit={handleSubmit}>
             <Card>
               <CardHeader
-                subheader='Please fill in the input fields to add a admin.'
-                title='Admin Information'
+                subheader="Please fill in the input fields to add a admin."
+                title="Admin Information"
               />
               <CardContent sx={{ pt: 0 }}>
                 <Box sx={{ m: -1.5 }}>
@@ -98,15 +113,15 @@ const AddNewAdminFields = () => {
                           error={Boolean(
                             touched.first_name && errors.first_name
                           )}
-                          label='First Name'
-                          name='first_name'
+                          label="First Name"
+                          name="first_name"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
                           value={values.first_name}
                         />
                         {touched.first_name && errors.first_name && (
-                          <FormHelperText error id='text-first-name'>
+                          <FormHelperText error id="text-first-name">
                             {errors.first_name}
                           </FormHelperText>
                         )}
@@ -124,15 +139,15 @@ const AddNewAdminFields = () => {
                           error={Boolean(
                             touched.middle_name && errors.middle_name
                           )}
-                          label='Middle Name (Optional)'
-                          name='middle_name'
+                          label="Middle Name (Optional)"
+                          name="middle_name"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
                           value={values.middle_name}
                         />
                         {touched.middle_name && errors.middle_name && (
-                          <FormHelperText error id='text-middle-name'>
+                          <FormHelperText error id="text-middle-name">
                             {errors.middle_name}
                           </FormHelperText>
                         )}
@@ -146,15 +161,15 @@ const AddNewAdminFields = () => {
                         <TextField
                           fullWidth
                           error={Boolean(touched.last_name && errors.last_name)}
-                          label='Last Name'
-                          name='last_name'
+                          label="Last Name"
+                          name="last_name"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
                           value={values.last_name}
                         />
                         {touched.last_name && errors.last_name && (
-                          <FormHelperText error id='text-last-name'>
+                          <FormHelperText error id="text-last-name">
                             {errors.last_name}
                           </FormHelperText>
                         )}
@@ -169,16 +184,16 @@ const AddNewAdminFields = () => {
                         <TextField
                           fullWidth
                           error={Boolean(touched.email && errors.email)}
-                          label='Email Address'
-                          name='email'
-                          type='email'
+                          label="Email Address"
+                          name="email"
+                          type="email"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
                           value={values.email}
                         />
                         {touched.email && errors.email && (
-                          <FormHelperText error id='text-email-address'>
+                          <FormHelperText error id="text-email-address">
                             {errors.email}
                           </FormHelperText>
                         )}
@@ -197,15 +212,15 @@ const AddNewAdminFields = () => {
                           error={Boolean(
                             touched.contact_number && errors.contact_number
                           )}
-                          label='Contact Number'
-                          name='contact_number'
+                          label="Contact Number"
+                          name="contact_number"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
                           value={values.contact_number}
                         />
                         {touched.contact_number && errors.contact_number && (
-                          <FormHelperText error id='text-contact-number'>
+                          <FormHelperText error id="text-contact-number">
                             {errors.contact_number}
                           </FormHelperText>
                         )}
@@ -220,8 +235,8 @@ const AddNewAdminFields = () => {
                         <TextField
                           fullWidth
                           error={Boolean(touched.gender && errors.gender)}
-                          label='Select Gender'
-                          name='gender'
+                          label="Select Gender"
+                          name="gender"
                           onBlur={handleBlur}
                           onChange={handleChange}
                           required
@@ -229,12 +244,12 @@ const AddNewAdminFields = () => {
                           SelectProps={{ native: true }}
                           value={values.gender}
                         >
-                          <option value='' disabled hidden></option>
-                          <option value='Male'>Male</option>
-                          <option value='Female'>Female</option>
+                          <option value="" disabled hidden></option>
+                          <option value="Male">Male</option>
+                          <option value="Female">Female</option>
                         </TextField>
                         {touched.gender && errors.gender && (
-                          <FormHelperText error id='text-gender'>
+                          <FormHelperText error id="text-gender">
                             {errors.gender}
                           </FormHelperText>
                         )}
@@ -244,13 +259,13 @@ const AddNewAdminFields = () => {
                 </Box>
               </CardContent>
               <Divider />
-              <CardActions sx={{ justifyContent: 'flex-end' }}>
+              <CardActions sx={{ justifyContent: "flex-end" }}>
                 <LoadingButton
                   loading={isLoading}
                   disableElevation
                   disabled={isSubmitting || !dirty || !isValid}
-                  type='submit'
-                  variant='contained'
+                  type="submit"
+                  variant="contained"
                   sx={{ backgroundColor: Colors.primaryColor }}
                 >
                   Save details
