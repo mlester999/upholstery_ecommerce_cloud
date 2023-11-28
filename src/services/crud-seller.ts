c14c5ad7faf12603d39efc537ec60b51266c921b
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-28 20:56:32 +0800

    Update crud-seller.ts

--- src/services/crud-seller.ts
+++ src/services/crud-seller.ts
@@ -45,7 +45,7 @@ interface UpdateSeller {
 export const crudSeller = createApi({
   reducerPath: 'crudSeller',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/',
+    baseUrl: process.env.API_URL,
     credentials: 'include',
   }),
   tagTypes: ['Sellers'],
