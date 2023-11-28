a7557b72976d3410245de77296dfc1fb24a1d020
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-28 20:56:24 +0800

    Update crud-seller-withdrawal.ts

--- src/services/crud-seller-withdrawal.ts
+++ src/services/crud-seller-withdrawal.ts
@@ -5,7 +5,7 @@ import { SellerWithdrawal } from '../models/SellerWithdrawal';
 export const crudSellerWithdrawal = createApi({
   reducerPath: 'crudSellerWithdrawal',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/',
+    baseUrl: process.env.API_URL,
     credentials: 'include',
   }),
   tagTypes: ['SellerWithdrawal', 'ERROR'],
