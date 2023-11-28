f1b926236b00afe554a520aca8ab68e25b85ad70
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-28 20:55:06 +0800

    Update crud-bank-account.ts

--- src/services/crud-bank-account.ts
+++ src/services/crud-bank-account.ts
@@ -15,7 +15,7 @@ interface UpdateBankAccount {
 export const crudBankAccount = createApi({
   reducerPath: 'crudBankAccount',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/',
+    baseUrl: process.env.API_URL,
     credentials: 'include',
   }),
   tagTypes: ['BankAccounts', 'ERROR'],
